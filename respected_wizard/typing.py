ee1171a0f1cccff9ec036f04d8eb3a7a58a09a4e
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-04-21 08:46:39 -0400

    Add docstrings

--- respected_wizard/typing.py
+++ respected_wizard/typing.py
@@ -6,24 +6,25 @@ from respected_wizard.exceptions import SchemaConversionException
 
 def resolve_type(type_name: str) -> Type:
     """
-    currently only supporting primitive types
+    Currently only supporting primitive types.
     """
-    if type_name == 'number' or type_name == 'float':
+    if type_name == "number" or type_name == "float":
         return float
-    elif type_name == 'integer':
+    elif type_name == "integer":
         return int
-    elif type_name == 'string':
+    elif type_name == "string":
         return str
-    elif type_name == 'boolean':
+    elif type_name == "boolean":
         return bool
-    elif type_name == 'array':
+    elif type_name == "array":
         return List
-    elif type_name == 'object':
+    elif type_name == "object":
         return Dict
     # TODO null?
     else:
-        raise SchemaConversionException('unrecognized type')
+        raise SchemaConversionException("unrecognized type")
 
 
 def is_optional_type(field: Type) -> bool:
+    """Check if type is Optional."""
     return get_origin(field) is Union and type(None) in get_args(field)
