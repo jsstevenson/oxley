af70c48b2716a57d40441b7801d0e54eaaf6137c
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-04-21 07:44:30 -0400

    Pull out typing tools into separate module

--- respected_wizard/class_builder.py
+++ respected_wizard/class_builder.py
@@ -1,5 +1,5 @@
 """Provide class construction tools."""
-from typing import Literal, Optional, Type, List, Dict, Type, ForwardRef, Union, get_args, get_origin
+from typing import Literal, Optional, Type, Dict, Type, ForwardRef
 from pathlib import Path
 import json
 import logging
@@ -9,6 +9,7 @@ from pydantic import create_model
 from pydantic.config import BaseConfig, Extra
 
 from respected_wizard.schema_versions import SchemaVersion, SCHEMA_MATCH_PATTERNS
+from respected_wizard.typing import resolve_type
 from respected_wizard.exceptions import SchemaConversionException, \
     UnsupportedSchemaException
 
@@ -214,28 +215,3 @@ class ClassBuilder:
             raise UnsupportedSchemaException(
                 'External references not currently supported')
         return ref.split(leader)[1]
-
-
-def resolve_type(type_name: str) -> Type:
-    """
-    currently only supporting primitive types
-    """
-    if type_name == 'number' or type_name == 'float':
-        return float
-    elif type_name == 'integer':
-        return int
-    elif type_name == 'string':
-        return str
-    elif type_name == 'boolean':
-        return bool
-    elif type_name == 'array':
-        return List
-    elif type_name == 'object':
-        return Dict
-    # TODO null?
-    else:
-        raise SchemaConversionException('unrecognized type')
-
-
-def is_optional(field: Type) -> bool:
-    return get_origin(field) is Union and type(None) in get_args(field)
---
+++ respected_wizard/typing.py
@@ -0,0 +1,29 @@
+"""Provide miscellaneous type utilities."""
+from typing import Type, List, Dict, Union, get_origin, get_args
+
+from respected_wizard.exceptions import SchemaConversionException
+
+
+def resolve_type(type_name: str) -> Type:
+    """
+    currently only supporting primitive types
+    """
+    if type_name == 'number' or type_name == 'float':
+        return float
+    elif type_name == 'integer':
+        return int
+    elif type_name == 'string':
+        return str
+    elif type_name == 'boolean':
+        return bool
+    elif type_name == 'array':
+        return List
+    elif type_name == 'object':
+        return Dict
+    # TODO null?
+    else:
+        raise SchemaConversionException('unrecognized type')
+
+
+def is_optional_type(field: Type) -> bool:
+    return get_origin(field) is Union and type(None) in get_args(field)
