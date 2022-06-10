8cd0926e5e11bcd4a4c88f5ebc751caa9116d780
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-06-10 15:38:25 -0400

    feat: resolve HTTP references (#22)

--- respected_wizard/exceptions.py
+++ respected_wizard/exceptions.py
@@ -8,6 +8,12 @@ class UnsupportedSchemaException(Exception):
     pass
 
 
+class InvalidReferenceException(Exception):
+    """Raise error when reference values can't be parsed."""
+
+    pass
+
+
 class SchemaConversionException(Exception):
     """Raise errors where valid JSONSchema elements may not fit into the Python type or
     object systems."""
